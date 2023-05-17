c4a3df978b574118c41877b36334badb7dba7235
Fatou <nfatouba05@gmail.com>
2023-05-17 14:56:58 +0000

    pls work

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,18 @@ import {Poppins} from "@next/font/google"
 import Footer from './components/Footer'
 import Nav from './components/Nav'
 import { useSession } from 'next-auth/react'
+
 const poppins = Poppins({
   subsets : ['latin'] ,
   weight: ['400','700','800'],
 })
 
 
-import { SessionProvider } from "next-auth/react"
-
 export default function App({ 
   Component, pageProps: {...pageProps }
  }) {
+
+ 
   
   return (
     
--- pages/episodes/EpisodeItems.jsx
+++ pages/episodes/EpisodeItems.jsx
@@ -1,3 +1,5 @@
+
+
 import { BookmarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Episodes from "../../episode.json"
